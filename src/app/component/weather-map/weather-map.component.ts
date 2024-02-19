2c36351394dbaa0da5396911fc2bc89a48f2230b
Nadeesha Dassanayake <nadeeshadassanayake96@gmail.com>
2024-02-19 14:06:13 +0530

    Added the map functioning and fixed some responsive issues with air quality data section

--- src/app/component/weather-map/weather-map.component.ts
+++ src/app/component/weather-map/weather-map.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet/src/lib/leaflet.module';
-import * as Leaflet from 'leaflet';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import * as L from 'leaflet';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-weather-map',
@@ -10,66 +11,39 @@ import * as Leaflet from 'leaflet';
   templateUrl: './weather-map.component.html',
   styleUrls: ['./weather-map.component.css'],
 })
-export class WeatherMapComponent {
-  map!: Leaflet.Map;
-  markers: Leaflet.Marker[] = [];
-  options = {
-    layers: [
-      Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }),
-    ],
-    zoom: 16,
-    center: { lat: 28.626137, lng: 79.821603 },
-  };
+export class WeatherMapComponent implements OnChanges {
+  ngOnChanges(change: SimpleChanges): void {
+    this.initMap();
+  }
 
-  initMarkers() {
-    const initialMarkers = [
-      {
-        position: { lat: 28.625485, lng: 79.821091 },
-        draggable: true,
-      },
-      {
-        position: { lat: 28.625293, lng: 79.817926 },
-        draggable: false,
-      },
-      {
-        position: { lat: 28.625182, lng: 79.81464 },
-        draggable: true,
-      },
-    ];
-    for (let index = 0; index < initialMarkers.length; index++) {
-      const data = initialMarkers[index];
-      const marker = this.generateMarker(data, index);
-      marker
-        .addTo(this.map)
-        .bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
-      this.map.panTo(data.position);
-      this.markers.push(marker);
+  @Input() weatherData: any;
+
+  map: L.Map;
+  radiusInMeters = 5000;
+
+  constructor() {}
+
+  initMap(): void {
+    if (this.weatherData != null && this.weatherData != undefined) {
+      if (this.map) {
+        this.map.remove();
+      }
+
+      const center = {
+        lat: this.weatherData?.coord?.lat,
+        lng: this.weatherData?.coord?.lon,
+      };
+      this.map = L.map('leafletMap').setView(center, 12);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors',
+      }).addTo(this.map);
+
+      L.circle(center, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.2,
+        radius: this.radiusInMeters,
+      }).addTo(this.map);
     }
-  }
-
-  generateMarker(data: any, index: number) {
-    return Leaflet.marker(data.position, { draggable: data.draggable })
-      .on('click', (event) => this.markerClicked(event, index))
-      .on('dragend', (event) => this.markerDragEnd(event, index));
-  }
-
-  onMapReady($event: Leaflet.Map) {
-    this.map = $event;
-    this.initMarkers();
-  }
-
-  mapClicked($event: any) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
-
-  markerClicked($event: any, index: number) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
-
-  markerDragEnd($event: any, index: number) {
-    console.log($event.target.getLatLng());
   }
 }
