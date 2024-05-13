4b4b154f9043562292666b117d80ea39aa60c25c
Nadeesha Dassanayake <nadeeshadassanayake96@gmail.com>
2024-05-13 14:07:57 +0530

    Fix imported dependency error

--- src/app/services/unsplash.service.ts
+++ src/app/services/unsplash.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Environment} from "../../environments/interface";
+import { Environment } from '../../environments/interface';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnsplashService {
   constructor(private http: HttpClient) {}
-  private keys : Environment = environment;
-  getPhoto(query:string){
-    const url:string = `https://api.unsplash.com/search/photos?client_id=${environment.apiUnsplash}&page=1&query=${query}`;
+  private keys: Environment = environment;
+  getPhoto(query: string) {
+    const url: string = `https://api.unsplash.com/search/photos?client_id=${environment.apiUnsplash}&page=1&query=${query}`;
     return this.http.get(url);
   }
 }
